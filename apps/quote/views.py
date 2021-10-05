571996fb34e2e0af65acdbe60fd94364a43a5a3a
NRamosD <nixonramosdue@gmail.com>
2021-10-04 23:42:52 -0500

    filtros en cotizar terminados

--- apps/quote/views.py
+++ apps/quote/views.py
@@ -173,15 +173,15 @@ def cotizar(request):
 
         if product_price_since != '' and product_price_since is not None and product_price_to != '' and product_price_to is not None:
             qs = qs.filter(price__gte=product_price_since, price__lte=product_price_to)
-        elif product_price_to == '' or product_price_to is None and product_price_since != '' and product_price_since is not None:
+        elif (product_price_to == '' or product_price_to is None) and (product_price_since != '' and product_price_since is not None):
             qs = qs.filter(price__gte=product_price_since)
-        elif product_price_since == '' or product_price_since is None and product_price_to != '' and product_price_to is not None:
+        elif (product_price_since == '' or product_price_since is None) and (product_price_to != '' and product_price_to is not None):
             qs = qs.filter(price__lte=product_price_to)
 
         if product_category != '' and product_category is not None:
-            qsCategory = Category.objects.filter(category_name__icontains=product_category)
+            qsCategory = Category.objects.filter(id_category_product = product_category) #__icontains
             try:
-                qs = qs.filter(id_supplier=qsCategory[0].id_supplier)
+                qs = qs.filter(id_category_product = qsCategory[0].id_category_product)
             except:
                 pass
 
